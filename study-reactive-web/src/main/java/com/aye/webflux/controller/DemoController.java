6533d6fde050d8c321372b537db7721ba86d4803
白伟 <m15677360304@163.com>
2021-07-13 11:47:35 +0800

    1.ReactiveWeb-->WebFlux编程 2.study-spring-project依赖本地spring5.0.3源码

---
+++ study-reactive-web/src/main/java/com/aye/webflux/controller/DemoController.java
@@ -0,0 +1,16 @@
+//package com.aye.webflux.controller;
+//
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/webflux")
+//public class DemoController {
+//
+//    @RequestMapping("/demo1/{name}")
+//    public void demo1(@PathVariable String name) {
+//        System.out.println("My Name Is " + name);
+//
+//    }
+//}
