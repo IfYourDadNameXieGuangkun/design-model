29f94fbc0996c9b20023f337dbc21746ec2c2548
白伟 <m15677360304@163.com>
2021-07-13 11:48:48 +0800

    1.ReactiveWeb-->WebFlux编程 2.study-spring-project依赖本地spring5.0.2源码

---
+++ study-reactive-web/src/main/java/com/aye/webflux/controller/WebFluxBeanController.java
@@ -0,0 +1,28 @@
+package com.aye.webflux.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+
+@Configuration
+public class WebFluxBeanController {
+    @Bean
+    public RouterFunction<ServerResponse> hellWebFlux() {
+        return RouterFunctions.route()
+                .GET("/webflux/{name}", serverRequest -> {
+                    String name = serverRequest.pathVariable("name");
+                    return ServerResponse.ok().bodyValue(name);
+                })
+                .POST("/webflux/hello", serverRequest -> {
+                    String name = serverRequest.exchange().getRequest().getQueryParams().getFirst("name");
+                    assert name != null;
+                    return ServerResponse.ok().bodyValue(name);
+                })
+                .build();
+    }
+}
--- study-redis-utils/src/main/java/com/study/redis/controller/BaseController.java
+++ study-redis-utils/src/main/java/com/study/redis/controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.study.redis.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.study.redis.conf.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @RestController
@@ -23,9 +26,9 @@ public class BaseController {
      * 根据key获取value
      */
     @RequestMapping("/get/{key}")
-    public void get(@PathVariable String key) {
-        String o = (String)redisDao.get(key);
-//        System.out.println(o);
+    public Object get(@PathVariable String key) {
+        Object o = redisDao.get(key);
+        return o;
     }
 
     /**
@@ -33,7 +36,8 @@ public class BaseController {
      */
     @RequestMapping("/keys")
     public void keys() {
-        Set<String> keys = redisDao.keys("PROMOTION:zy:MZHE*");
+//        Set<String> keys = redisDao.keys("PROMOTION:zy:MZHE*");
+        Set<String> keys = redisDao.keys("PROMOTION:OR*");
         if (!CollectionUtils.isEmpty(keys)){
             System.out.println(keys);
         }
@@ -46,5 +50,46 @@ public class BaseController {
     public void hget(@PathVariable String key,@PathVariable String filed) {
         Object obj = redisDao.hget(key, filed);
         System.out.println(obj);
+    }
+
+    /**
+     * 获取所有的前缀匹配的keys值
+     */
+    @RequestMapping("/set")
+    public void set() {
+        Set<String> objects = new HashSet<>();
+        objects.add("1249601977045916729");
+        objects.add("12341234");
+//        redisDao.set("PROMOTION:zy:MZHE:3011", objects.toString());
+        String s = (String) redisDao.get("PROMOTION:zy:MZHE:3011");
+        Set<String> promIds = JSON.parseObject(s, new TypeReference<Set<String>>() {
+        });
+        System.out.println(promIds.toString());
+    }
+
+    /**
+     * 获取所有的前缀匹配的keys值
+     */
+    @RequestMapping("/del")
+    public void del() {
+        Set<String> keys = redisDao.keys("PROMOTION:zy:MZHE*");
+        int cnt = 0;
+        for (String key : keys) {
+            redisDao.del(key);
+            System.out.println(++cnt+"==>"+key);
+        }
+    }
+
+    public static void main(String[] args) {
+        Set<String> validPromotionIds = new HashSet<>();
+        Set<String> promIds = new HashSet<>();
+        promIds.add("123");
+        promIds.add("444");
+        promIds.add("555");
+        validPromotionIds.add("123");
+        validPromotionIds.add("234");
+        Set<String> result = new HashSet<>(validPromotionIds);
+        result.retainAll(promIds);
+        System.out.println(result.toString());
     }
 }
